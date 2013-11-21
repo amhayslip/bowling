5d9bd8ae8d67e64e16d9c58d75e11692e8245423
amhayslip <amhayslip@gmail.com>
2013-11-21 16:07:07 -0600

    Edited score method, tests passed

--- bowling_spec.rb
+++ bowling_spec.rb
@@ -3,7 +3,7 @@ require 'bundler/setup'
 require 'rspec'
 require_relative 'bowling'
 
-	describe BowlingGame do
+describe BowlingGame do
 		let(:game) { BowlingGame.new }
 
 	 describe '.initialize' do
@@ -14,18 +14,36 @@ require_relative 'bowling'
 	
 
 
-	describe '#score' do
+describe '#score' do
+	context 'when only gutter balls are thrown' do
 	  it 'scores the game as 0 when only gutter balls are thrown' do
 	    20.times { game.roll(0) }
 
 	    expect(game.score).to eq(0)
 	  end
+	end
+	context 'when one pin is knocked down on each roll' do
 	  it 'scores the game as 20 when one pin is knocked down on each roll' do
 	    20.times { game.roll(1) }
 
 	    expect(game.score).to eq(20)
 	  end
-	end
+	 end
+	 context 'when a spare is thrown' do
+  	   it 'records the frame score as 10 plus the number of pins knocked down in the next roll' do
+         3.times  { game.roll(5) }
+         17.times { game.roll(0) }
+
+        expect(game.score).to eq(20)
+      end
+    end
+  end
+
+
+
+
+
+
 end
 
 
