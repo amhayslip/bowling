d4611138be5b2d31ace8b884532e3dea1a418a22
Laney <laney.bevill@gmail.com>
2013-11-21 17:22:12 -0600

    refactoring with 4 failing tests

--- bowling.rb
+++ bowling.rb
@@ -3,6 +3,8 @@ class BowlingGame
 
 	def initialize
 		@rolls = []
+		@total_score  = 0
+		@current_roll = 0
 	end
 
 	# Record a roll in the game.
@@ -17,30 +19,64 @@ class BowlingGame
 	# Returns the Integer score for this game. Expects to be run after all rolls
 	# for the game have been recorded.
 	# Returns the Integer score for this game. Expects to be run after all rolls
-# for the game have been recorded.
+	# for the game have been recorded.
 	def score
-	  total_score  = 0
-	  current_roll = 0
 
 	  while current_roll < @rolls.size
-	    roll      = @rolls[current_roll]
-	    next_roll = @rolls[current_roll + 1]
+	  	init_roll
 
-	    if roll == 10
-	      total_score += 10 + next_roll + @rolls[current_roll + 2]
-	      current_roll += 1
-	    elsif roll + next_roll == 10
-	      total_score += 10 + @rolls[current_roll + 2]
-	      current_roll += 2
-	    else
-	      total_score += roll + next_roll
-	      current_roll += 2
+	    if strike?
+	    	score_strike
+	    elsif spare?
+	    	score_spare
+	    else less_than_spare?
+	    	score_less_than_spare
 	    end
 	  end
 
 	  return total_score
 	end
-	
 
+	
+	def init_roll
+	    @roll      = @rolls[@current_roll]
+	    @next_roll = @rolls[@current_roll + 1]
+	  end
+
+	  # Returns true if the current roll is a strike, false otherwise.
+	  def strike?
+	    @roll == 10
+	  end
+	  # Scores a strike frame, and adds it to the total score for the game.
+	  #
+	  # Returns nothing.
+	  def score_strike
+	    @total_score += 10 + @next_roll + @rolls[@current_roll + 2]
+	    @current_roll += 1
+	  end
+
+	  #returns true if the current frame is a spare
+	  def spare?
+	  	@roll + next_roll == 10
+	  end
+	  # Scores a spare frame, and adds it to the total score for the game.
+	  #
+	  # Returns nothing.
+	  def score_spare
+	  	@total_score += 10 + @rolls[current_roll + 2]
+	  	@current_roll += 2
+	  end
+
+	  #returns true if you get less than a spare
+	  def less_than_spare?
+	  	@roll + next_roll < 10
+	  end
+
+	  # scores a frame that is worse than a spare
+	  # returns nothing.
+	  def score_less_than_spare
+	  	@total_score += roll + next_roll
+	  	@current_roll += 2
+	  end
 
 end
